b8432a1047d556cccd3c0c7d9c8b8599ddf5659d
Trihedraf <forrest@mailfsh.com>
2025-07-09 06:38:56 -0700

    Create ps1 to setup CommitMono font

---
+++ commitmono.ps1
@@ -0,0 +1,8 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process pwsh.exe -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+    exit;
+}
+Start-BitsTransfer https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CommitMono.zip $env:TMP\CommitMono.zip
+Expand-Archive -Path $env:TMP\CommitMono.zip -DestinationPath $env:SystemRoot\Fonts\ -Force
+reg import commitmono.reg
+rm $env:TMP\CommitMono.zip
